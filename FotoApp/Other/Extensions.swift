8711c82e3d62842e6d9e491315542591d61cd3f2
Valerio <valerioly@gmail.com>
2018-11-28 04:02:25 -0800

    localizable caricato r.swift caricato

---
+++ FotoApp/Other/Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Extensions.swift
+//  FotoApp
+//
+//  Created by Valerio Ly on 28/11/18.
+//  Copyright © 2018 Valerio Ly. All rights reserved.
+//
+import UIKit
+import Foundation
+
+extension String {
+    
+    var localized: String {
+        return NSLocalizedString(self, comment: "")
+}
+}
