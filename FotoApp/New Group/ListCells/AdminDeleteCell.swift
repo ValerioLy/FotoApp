d46b625ee4b342414ba35fb9ab2527d283597026
Nicola Pagiaro <pagiaro@gmail.com>
2019-01-14 12:40:07 +0100

    Admin delete album done

---
+++ FotoApp/New Group/ListCells/AdminDeleteCell.swift
@@ -0,0 +1,23 @@
+//
+//  AdminDeleteCell.swift
+//  FotoApp
+//
+//  Created by Nicola on 14/01/19.
+//
+
+import UIKit
+
+class AdminDeleteCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
